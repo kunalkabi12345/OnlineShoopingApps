fa5c356b43953d12b77e7adcc4dd90178ea08ef3
k <k@DESKTOP-A3TO8VT>
2021-12-23 09:33:56 +0530

    [WebTest]First Servlet Added

---
+++ WebAppTest/src/ServletTest.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+@WebServlet("/ServletTest")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Welcome to My Web App Test Page...");
+	}
+
+}
